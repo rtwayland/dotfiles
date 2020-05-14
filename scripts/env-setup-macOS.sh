51b469a5be0db61d443531fc7306147e04a4b54f
Raleigh Wayland <raleigh.wayland@stormapp.com>
2020-05-14 12:58:34 -0600

    Moving setup scripts around

--- scripts/env-setup-macOS.sh
+++ scripts/env-setup-macOS.sh
@@ -1,17 +1,3 @@
-# Setup xcode tools
-echo "[SCRIPTS] Install Xcode Tools"
-xcode-select —-install
-# Install Homebrew
-echo "[SCRIPTS] Install Homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# Install zsh, ohmyzsh, and change shell
-echo "[SCRIPTS] Install ZSH"
-brew install zsh
-echo "[SCRIPTS] Install Oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "[SCRIPTS] Upgrade Oh-my-zsh"
-upgrade_oh_my_zsh
-# chsh -s `which zsh`
 # Set up zsh syntax highlighting, autosuggestions, and nvm
 echo "[SCRIPTS] Install zsh plugins"
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting 
@@ -33,5 +19,3 @@ rm ~/.zshrc && ./install
 # iTerm2 Setup
 echo "[SCRIPTS] Download terminal font"
 open https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Meslo.zip
-# Directory Setup
-cd && mkdir Workspace
--- scripts/macOS-setup.sh
+++ scripts/macOS-setup.sh
@@ -1,4 +1,14 @@
 # Setup xcode tools
+echo "[SCRIPTS] Install Xcode Tools"
 xcode-select —-install
 # Install Homebrew
+echo "[SCRIPTS] Install Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install zsh, ohmyzsh, and change shell
+echo "[SCRIPTS] Install ZSH"
+brew install zsh
+echo "[SCRIPTS] Install Oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "[SCRIPTS] Upgrade Oh-my-zsh"
+upgrade_oh_my_zsh
+# chsh -s `which zsh`
