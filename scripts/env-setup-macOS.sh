80789a23b5d7dedafdb6e2d878e0585fbfc30691
Raleigh Wayland <raleigh.wayland@stormapp.com>
2020-07-31 14:45:34 -0600

    Minor changes

--- scripts/env-setup-macOS.sh
+++ scripts/env-setup-macOS.sh
@@ -1,8 +1,12 @@
 # Set up zsh syntax highlighting, autosuggestions, and nvm
 echo "[SCRIPTS] Install zsh plugins"
+# zsh-syntax-highlighting 
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting 
+# zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+# zsh-nvm
 git clone git://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+# powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "[SCRIPTS] Install cli tools"
 brew install tmux
@@ -18,5 +22,5 @@ git fetch && git pull
 echo "[SCRIPTS] Execute install script"
 rm ~/.zshrc && ./install
 # iTerm2 Setup
-echo "[SCRIPTS] Download terminal font"
-open https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Meslo.zip
+# echo "[SCRIPTS] Download terminal font"
+# open https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Meslo.zip
