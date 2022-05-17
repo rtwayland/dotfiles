2d7b53f19718af49130e2ec72a9e20b95b3750aa
Raleigh Wayland <rtwayland@gmail.com>
2022-05-17 09:29:32 -0600

    Updating init script
    Newer versions of macOS have zsh installed by default

--- scripts/macOS-setup.sh
+++ scripts/macOS-setup.sh
@@ -3,10 +3,7 @@ echo "[SCRIPTS] Install Xcode Tools"
 xcode-select —-install
 # Install Homebrew
 echo "[SCRIPTS] Install Homebrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# Install zsh, ohmyzsh, and change shell
-echo "[SCRIPTS] Install ZSH"
-brew install zsh
-echo "[SCRIPTS] Install Oh-my-zsh"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install ohmyzsh
+echo "[SCRIPTS] Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# chsh -s `which zsh`
