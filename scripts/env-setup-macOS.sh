6b2484ae14d4209e88e7849562ce3d9e404c413f
Raleigh Wayland <rtwayland@gmail.com>
2022-05-17 09:08:20 -0600

    Updating repo links/paths

--- scripts/env-setup-macOS.sh
+++ scripts/env-setup-macOS.sh
@@ -1,15 +1,15 @@
 # Set up zsh syntax highlighting, autosuggestions, and nvm
 echo "[SCRIPTS] Install zsh plugins"
-# zsh-syntax-highlighting 
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting 
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh-nvm
-git clone git://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
+git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "[SCRIPTS] Install cli tools"
-brew install tmux
+# brew install tmux
 brew install z
 brew install neovim
 brew install python
