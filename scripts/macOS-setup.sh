0575d6ae5a2a6dc6dd82f5313bd04df998a65c1b
Raleigh Wayland <rtwayland@gmail.com>
2022-05-17 12:25:57 -0600

    Adding additional homebrew setup scripts

--- scripts/macOS-setup.sh
+++ scripts/macOS-setup.sh
@@ -4,6 +4,8 @@ xcode-select —-install
 # Install Homebrew
 echo "[SCRIPTS] Install Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/raleigh.wayland/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 # Install ohmyzsh
 echo "[SCRIPTS] Install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
