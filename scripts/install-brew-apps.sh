3ff426e562d26eeb3751daf7996271ace2eaf475
Raleigh Wayland <rtwayland@gmail.com>
2022-05-18 15:16:26 -0600

    Updating list of apps

--- scripts/install-brew-apps.sh
+++ scripts/install-brew-apps.sh
@@ -1,33 +1,27 @@
-# Everyday
-brew cask install google-chrome
-brew cask install homebrew/cask-versions/firefox-developer-edition
-brew cask install slack
-brew cask install nordvpn
-# Productivity
-brew cask install alfred
-brew cask install rectangle
-brew cask install homebrew/cask-drivers/logitech-options
-brew cask install appcleaner
-brew cask install macmediakeyforwarder
-brew cask install dropbox
-brew cask install signal
-brew cask install zoomus
-brew cask install notion
-# brew cask install karabiner-elements
-# Dev
-brew cask install iterm2
-brew cask install visual-studio-code
-brew cask install postman
-# Design
-# brew cask install autodesk-fusion360
-# brew cask install qcad
+# Browsers
+brew install --cask  google-chrome
+brew install --cask  homebrew/cask-versions/firefox-developer-edition
+# Communication
+brew install --cask  slack
+brew install --cask  signal
+brew install --cask  zoom
+# Computer Management
+brew install --cask  alfred
+brew install --cask  rectangle
+brew install --cask  karabiner-elements
+brew install --cask  homebrew/cask-drivers/logitech-options
+brew install --cask  appcleaner
+# Development
+brew install --cask  iterm2
+brew install --cask  visual-studio-code
+brew install --cask  postman
+brew install --cask  tableplus
 # Entertainment
-brew cask install spotify
-brew cask install discord
-brew cask install calibre
-brew cask install scrivener
-
-# Non Brew Apps
-# Day One
-# Kiwi for Gmail
-# Transporter
+brew install --cask  spotify
+# Personal
+brew install --cask discord
+brew install --cask calibre
+brew install --cask scrivener
+## CAD Design
+# brew install --cask autodesk-fusion360
+# brew install --cask qcad
